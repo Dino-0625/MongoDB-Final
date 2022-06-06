e2b1f69fe1af994a1ff46b6bdf1c0fc9eedad1c0
lemon37564 <stonydragon0701@gmail.com>
2022-06-06 22:45:12 +0800

    write frontend

--- webpage/index.js
+++ webpage/index.js
@@ -1,12 +1,20 @@
 let submitButton;
+let nicknameInput;
+let hiddenFrame;
 
 function start() {
     submitButton = document.getElementById("submit-button");
     submitButton.addEventListener("click", registerUser, false);
+
+    nicknameInput = document.getElementById("nickname");
+    hiddenFrame = document.getElementById("hidden-frame");
 }
 
 function registerUser() {
     console.log("register");
+    document.getElementById("form-nickname").value = nicknameInput.value;
+    document.getElementById("reg-form").submit();
+    window.location = "/chat"
 }
 
 window.addEventListener("load", start);
