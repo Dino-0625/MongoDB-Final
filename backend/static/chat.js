9a3beae53e61bd5f736bba87a7f95128e1a5e11e
Dino <dinolee0625xd@gmail.com>
2022-06-07 22:56:52 +0800

    eventlistener enter add

--- backend/static/chat.js
+++ backend/static/chat.js
@@ -3,44 +3,50 @@ let messageInput;
 let messageButton;
 
 function httpGet(theUrl) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", theUrl, false); // false for synchronous request
-    xmlHttp.send(null);
-    return xmlHttp.responseText;
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", theUrl, false); // false for synchronous request
+  xmlHttp.send(null);
+  return xmlHttp.responseText;
 }
 
 function sendMessage() {
-    let xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("POST", "/message");
-    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xmlhttp.onload = function () {
-        reloadChatRoom();
-    }
-    xmlhttp.send("user_id=" + getUserId() + "&message=" + messageInput.value);
-    messageInput.value = "";
+  let xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("POST", "/message");
+  xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xmlhttp.onload = function () {
+    reloadChatRoom();
+  };
+  xmlhttp.send("user_id=" + getUserId() + "&message=" + messageInput.value);
+  messageInput.value = "";
 }
 
 function getUserId() {
-    return localStorage.getItem("user-id");
+  return localStorage.getItem("user-id");
 }
 
 function reloadChatRoom() {
-    let messagesText = httpGet("/message");
-    let messages = JSON.parse(messagesText);
-    let html = "";
-    for (let i = 0; i < messages.length; i++) {
-        html = "<label>" + messages[i]["msg"] + "</label><br>" + html;
-    }
-    messageBox.innerHTML = html;
+  let messagesText = httpGet("/message");
+  let messages = JSON.parse(messagesText);
+
+  let html = "";
+  for (let i = 0; i < messages.length; i++) {
+    html = "<label>" + messages[i]["msg"] + "</label><br>" + html;
+  }
+  messageBox.innerHTML = html;
 }
 
 function start() {
-    messageBox = document.getElementById("message-box");
-    messageInput = document.getElementById("message-input");
-    messageButton = document.getElementById("send-button");
-    messageButton.addEventListener("click", sendMessage, false);
-    reloadChatRoom();
-    setInterval(reloadChatRoom, 300);
+  messageBox = document.getElementById("message-box");
+  messageInput = document.getElementById("message-input");
+  messageButton = document.getElementById("send-button");
+  messageButton.addEventListener("click", sendMessage, false);
+  messageInput.addEventListener("keypress", function (e) {
+    if (e.key == "Enter") {
+      messageButton.click();
+    }
+  });
+  reloadChatRoom();
+  setInterval(reloadChatRoom, 100);
 }
 
 window.addEventListener("load", start);
--- backend/static/index.js
+++ backend/static/index.js
@@ -2,23 +2,22 @@ let submitButton;
 let nicknameInput;
 
 function start() {
-    submitButton = document.getElementById("submit-button");
-    submitButton.addEventListener("click", registerUser, false);
-
-    nicknameInput = document.getElementById("nickname");
+  submitButton = document.getElementById("submit-button");
+  submitButton.addEventListener("click", registerUser, false);
+  nicknameInput = document.getElementById("nickname");
 }
 
 // do http post
 function registerUser() {
-    let xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("POST", "/user");
-    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xmlhttp.onload = function () {
-        localStorage.setItem("user-id", xmlhttp.responseText);
-        // redirect to chat room
-        window.location = "/chat";
-    }
-    xmlhttp.send("nickname=" + nicknameInput.value);
+  let xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("POST", "/user");
+  xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xmlhttp.onload = function () {
+    localStorage.setItem("user-id", xmlhttp.responseText);
+    // redirect to chat room
+    window.location = "/chat";
+  };
+  xmlhttp.send("nickname=" + nicknameInput.value);
 }
 
 window.addEventListener("load", start);
