e3b5734b74a37f8fe15dada7e8edf6ef8e34614a
lemon37564 <stonydragon0701@gmail.com>
2022-06-09 17:26:16 +0800

    add backend APIs

--- mongoch/__init__.py
+++ mongoch/__init__.py
@@ -9,7 +9,7 @@ import database
 app = flask.Flask(__name__, static_url_path='')
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
     return app.send_static_file('index.html')
 
@@ -21,7 +21,13 @@ def user_reg():
     return flask.Response(response=id, status=http.HTTPStatus.OK)
 
 
-@app.route('/chat', methods=['GET', 'POST'])
+@app.route('/user', methods=['GET'])
+def user_info():
+    user_id = flask.request.args.get("user_id")
+    return database.user.get_info(user_id)
+
+
+@app.route('/chat', methods=['GET'])
 def chat():
     return app.send_static_file('chat.html')
 
@@ -38,6 +44,18 @@ def message_post():
     msg = html.escape(msg)
     database.message.insert(user_id, msg)
     return flask.Response(response="", status=http.HTTPStatus.OK)
+
+
+@app.route('/analysis', methods=['GET'])
+def analysis():
+    user_id = flask.request.args.get("user_id", "", type=str)
+    if user_id == "":
+        return flask.Response(response="", status=http.HTTPStatus.BAD_REQUEST)
+    return {
+        "total_msg_count": database.message.get_count_of_everyone(),
+        "total_char_count": database.message.get_char_count_of_everyone(),
+        "avg_msg_len": database.message.get_avg_msg_len_of_everyone(),
+    }
 
 
 def run():
