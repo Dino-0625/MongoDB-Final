c95e0a33b091f9c43861fda392efc975bd4db1c8
lemon37564 <stonydragon0701@gmail.com>
2022-06-09 16:51:19 +0800

    add more database functionality

--- database/message.py
+++ database/message.py
@@ -13,7 +13,8 @@ collection = database.db.message
 def message_get_all() -> list:
     """retur all message"""
     # from new to old
-    msg_list = collection.find().sort("_id", pymongo.DESCENDING).limit(MAX_MSG_SHOW_COUNT)
+    msg_list = collection.find().sort(
+        "_id", pymongo.DESCENDING).limit(MAX_MSG_SHOW_COUNT)
     return list(msg_list)
 
 
@@ -25,11 +26,55 @@ def message_get_between_dates(date_begin: datetime.datetime, date_end: datetime.
 
 
 def message_get_count_of_user(user_id: str) -> int:
-    return len(list(collection.find({"user_id": user_id})))
+    return collection.count_documents({"user_id": user_id})
+
+
+def message_get_char_count_of_user(user_id: str) -> int:
+    query = [
+        {
+            "$match": {
+                "user_id": user_id,
+            }
+        },
+        {
+            "$group": {
+                "_id": "null",
+                "total": {
+                    "$sum": {"$strLenCP": "$msg"},
+                },
+            }
+        }
+    ]
+    try:
+        result = list(collection.aggregate(query))
+        return result[0]["total"]
+    except Exception as err:
+        logging.warning("Getting message char count: {}".format(err))
+        return -1
 
 
 def message_get_avg_character_count_of_user(user_id: str) -> int:
-    return collection.aggregate
+    query = [
+        {
+            "$match": {
+                "user_id": user_id,
+            }
+        },
+        {
+            "$group": {
+                "_id": "null",
+                "avg": {
+                    "$avg": {"$strLenCP": "$msg"},
+                },
+            }
+        }
+    ]
+    try:
+        result = list(collection.aggregate(query))
+        return result[0]["avg"]
+    except Exception as err:
+        logging.warning("Getting message char avg: {}".format(err))
+        return -1
 
 
 def message_insert(user_id: str, msg: str) -> bool:
@@ -37,31 +82,35 @@ def message_insert(user_id: str, msg: str) -> bool:
     # check if there's user or not
     user_name = database.user_id_to_nickname(user_id)
     if user_name == "":
-        logging.warning("Failed to insert message because there is no user with id \"{}\"".format(user_id))
+        logging.warning(
+            "Failed to insert message because there is no user with id \"{}\"".format(user_id))
         return False
-    
-    logging.info("Inserting message \"{}\" from user \"{}\"".format(msg, user_name))
-    
+
+    logging.info(
+        "Inserting message \"{}\" from user \"{}\"".format(msg, user_name))
+
     # cannot send empty string
     if len(msg) == 0:
         logging.info("Empty message is rejected")
         return False
 
     post = {
-        "_id": _find_next_id(), 
+        "_id": _find_next_id(),
         "user_id": user_id,
         "user_name": user_name,
         "msg": msg,
         "msg_len": len(msg),
         "date": datetime.datetime.now().isoformat(),
-        }
+    }
 
     try:
         post_id = collection.insert_one(post).inserted_id
-        logging.info("Message \"{}\" inserted successfully with id {}".format(msg, post_id))
+        logging.info(
+            "Message \"{}\" inserted successfully with id {}".format(msg, post_id))
         return True
     except Exception as err:
-        logging.warning("Cannot insert chat message due to the following error:\n{}".format(err))
+        logging.warning(
+            "Cannot insert chat message due to the following error:\n{}".format(err))
         return False
 
 
@@ -80,11 +129,13 @@ def _find_next_id() -> int:
 def message_delete_one(user_id: str, msg_id: int) -> bool:
     """delete one message"""
     try:
-        logging.info("Deleting message \"{}\" from user \"{}\"".format(msg_id, user_id))
+        logging.info(
+            "Deleting message \"{}\" from user \"{}\"".format(msg_id, user_id))
         collection.delete_one({"user_id": user_id, "_id": msg_id})
         return True
     except Exception as err:
-        logging.warning("Cannot delete message from user \"{}\" due to the following error:{}\n".format(user_id, err))
+        logging.warning(
+            "Cannot delete message from user \"{}\" due to the following error:{}\n".format(user_id, err))
         return False
 
 
@@ -95,7 +146,8 @@ def message_delete_all(user_id: str) -> bool:
         collection.delete_many({"user_id": user_id})
         return True
     except Exception as err:
-        logging.warning("Cannot delete all messages from user \"{}\" due to the following error:{}\n".format(user_id, err))
+        logging.warning(
+            "Cannot delete all messages from user \"{}\" due to the following error:{}\n".format(user_id, err))
         return False
 
 
@@ -103,8 +155,12 @@ def message_edit(user_id: str, msg_id: int, new_msg: str) -> bool:
     """edit one message from one user"""
     try:
         logging.info("Editing message from user \"{}\"".format(user_id))
-        collection.update_one({"user_id": user_id, "_id": msg_id}, {"$set": {"msg": new_msg}})
+        collection.update_one(
+            {"user_id": user_id, "_id": msg_id},
+            {"$set": {"msg": new_msg}},
+        )
         return True
     except Exception as err:
-        logging.warning("Cannot edit messages from user \"{}\" due to the following error:{}\n".format(user_id, err))
+        logging.warning(
+            "Cannot edit messages from user \"{}\" due to the following error:{}\n".format(user_id, err))
         return False
--- database/user.py
+++ database/user.py
@@ -32,29 +32,54 @@ def user_add(nickname: str) -> str:
     while (row != None) and (len(row) != 0):
         user_id = _random_id()
 
-    post = {"_id": user_id, "nickname": nickname, "reg_date": datetime.datetime.now().isoformat()}
+    post = {
+        "_id": user_id,
+        "nickname": nickname,
+        "reg_date": datetime.datetime.now().isoformat(),
+    }
     try:
         post_id = collection.insert_one(post).inserted_id
-        logging.info("User \"{}\" inserted successfully with id \"{}\"".format(nickname, post_id))
+        logging.info(
+            "User \"{}\" inserted successfully with id \"{}\"".format(nickname, post_id))
         return user_id
     except Exception as err:
-        logging.warning("Cannot insert user due to the following error:\n{}".format(err))
+        logging.warning(
+            "Cannot insert user due to the following error:\n{}".format(err))
         return ""
 
 
-def user_get_info() -> dict:
-    return {}
+def user_get_info(user_id: str) -> dict:
+    """get user information"""
+    logging.info("Getting info of user \"{}\"".format(user_id))
+    try:
+        info_norm = collection.find_one({"_id": user_id})
+        info = dict(info_norm)
+        info.update(
+            total_msg_count=database.message_get_count_of_user(user_id),
+            totoal_char_count=database.message_get_char_count_of_user(user_id),
+            avg_msg_len=database.message_get_avg_character_count_of_user(
+                user_id),
+        )
+        return info
+    except Exception as err:
+        logging.warning(
+            "Cannot get info of user \"{}\" due to error: {}".format(user_id, err))
+        return dict()
 
 
 def user_change_name(user_id: str, new_name: str) -> bool:
     """change user nickname"""
-    logging.info("User \"{}\" is changing nickname to \"{}\"".format(user_id, new_name))
+    logging.info(
+        "User \"{}\" is changing nickname to \"{}\"".format(user_id, new_name))
     try:
-        collection.update_one({"_id": user_id}, {"$set": {"nickname": new_name}})
-        logging.info("User \"{}\" nickname changed successfully".format(user_id))
+        collection.update_one(
+            {"_id": user_id}, {"$set": {"nickname": new_name}})
+        logging.info(
+            "User \"{}\" nickname changed successfully".format(user_id))
         return True
     except Exception as err:
-        logging.log("User \"{}\" nickname change failed with following error:\n{}".format(user_id, err))
+        logging.log(
+            "User \"{}\" nickname change failed with following error:\n{}".format(user_id, err))
         return False
 
 
@@ -68,9 +93,10 @@ def user_delete(user_id: str) -> bool:
         logging.info("User \"{}\" deleted successfully".format(user_id))
         return True
     except Exception as err:
-        logging.log("User \"{}\" failed to delete with following error:\n{}".format(user_id, err))
+        logging.log(
+            "User \"{}\" failed to delete with following error:\n{}".format(user_id, err))
         return False
-        
+
 
 def user_id_to_nickname(user_id: str) -> str:
     """return user nickname from id"""
